8ee2b84952bf71ee721c837af8afb19b0033ec82
chuhx <chuhx1024@126.com>
2022-02-17 15:10:24 +0800

    feat: redux init

--- src/components/core/Home.tsx
+++ src/components/core/Home.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 import Layout from './Layout'
 
 const Home = () => {
+    const state = useSelector(state=> state)
   return (
-    <Layout>Home</Layout>
+    <Layout>Home{JSON.stringify(state)}</Layout>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
 import App from './App';
 import { API } from './config';
+import store from './store/index'
 
 console.log(API)
 
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
   document.getElementById('root')
 );
 
